a15b441173aed25c219dbf8e3d24c0fcec3693ab
Anita Metlova <anitametlova@gmail.com>
2024-05-24 16:55:53 -0400

    added promise callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,17 +57,68 @@ public_users.post("/register", (req,res) => {
         return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+
+
+/*// Get the book list available in the shop
 public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books,null,4));
 });
+*/
 
+// Get book list available in the shop using Promise callback
+public_users.get('/', function (req, res) {
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(books);
+        }, 0); 
+    });
+
+    myPromise
+        .then(bookList => {
+            res.send(JSON.stringify(bookList, null, 4));
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+
+
+/*
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn= req.params.isbn;
     res.send(books[isbn])
  });
-  
+*/
+
+// Get book details based on ISBN using Promise callback
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const book = books[isbn];
+            if (book) {
+                resolve(book);
+            } else {
+                reject('Book not found');
+            }
+        }, 0); 
+    });
+
+    myPromise
+        .then(book => {
+            res.send(book);
+        })
+        .catch(error => {
+            res.status(404).json({ error: 'Book not found' });
+        });
+});
+
+
+
+/*
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
@@ -75,11 +126,64 @@ public_users.get('/author/:author',function (req, res) {
     res.send(JSON.stringify(booksByAuthor));
 });
 
+*/
+
+// Get books by author using Promise callback
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const booksByAuthor = getBooksByAuthor(author);
+            if (booksByAuthor.length > 0) {
+                resolve(booksByAuthor);
+            } else {
+                reject('No books found for the author');
+            }
+        }, 0);
+    });
+
+    myPromise
+        .then(books => {
+            res.send(JSON.stringify(books));
+        })
+        .catch(error => {
+            res.status(404).json({ error: 'Author not found or no books found for the author' });
+        });
+});
+
+
+/*
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     const booksByTitle = getBooksByTitle(title);
     res.send(JSON.stringify(booksByTitle));
+});
+*/
+
+// Get all books based on title using Promise callback
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const booksByTitle = getBooksByTitle(title);
+            if (booksByTitle.length > 0) {
+                resolve(booksByTitle);
+            } else {
+                reject('No books found with the given title');
+            }
+        }, 0); 
+    });
+
+    myPromise
+        .then(books => {
+            res.send(JSON.stringify(books));
+        })
+        .catch(error => {
+            res.status(404).json({ error: 'No books found with the given title' });
+        });
 });
 
 //  Get book review
